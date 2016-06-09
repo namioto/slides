1e028ae0d43c6a1ed04756c81e143d7485ed0cf1
박대성 <namioto@gmail.com>
2016-06-10 02:04:21 +0900

    no message

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function initReveal(){
         // Optional libraries used to extend on reveal.js
         dependencies: [
             { src: '/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-            { src: '/reveal.js/plugin/markdown/showdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+            { src: '/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
             { src: '/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
             { src: '/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
             { src: '/reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
@@ -22,6 +22,7 @@ function initReveal(){
             // { src: 'plugin/remotes/remotes.js', async: true, condition: function() { return !!document.body.classList; } }
         ]
     });
+
 }
 
 
